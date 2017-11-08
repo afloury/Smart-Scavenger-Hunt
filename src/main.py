bd5cb2bd59cac13b5a3d4c3ac9321e18877494c3
Nicolas Lim <nicnl25@gmail.com>
2017-11-08 16:21:55 +0100

    Code de bootstrap pour le routeur

---
+++ src/main.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+from flask import Flask, request
+from flask_cors import CORS
+import os
+import random
+import uuid
+import requests
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/hello/', methods=['GET'])
+def list_all_nodes():
+    return 'salut'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001)
