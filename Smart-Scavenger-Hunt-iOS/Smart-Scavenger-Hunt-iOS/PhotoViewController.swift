6fba68f92a80524c395a7dd889dad40795dbffc3
afloury <antoine.floury@imerir.com>
2017-11-09 21:13:14 +0100

    Photo: - Send photo to the server - get location from the phone

--- Smart-Scavenger-Hunt-iOS/Smart-Scavenger-Hunt-iOS/PhotoViewController.swift
+++ Smart-Scavenger-Hunt-iOS/Smart-Scavenger-Hunt-iOS/PhotoViewController.swift
@@ -1,17 +1,64 @@
 import UIKit
 import AVFoundation
+import Alamofire
+import SwiftyJSON
+import Photos
 
-class PhotoViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, AVCaptureMetadataOutputObjectsDelegate, AVCaptureVideoDataOutputSampleBufferDelegate {
+class PhotoViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, AVCaptureMetadataOutputObjectsDelegate, AVCaptureVideoDataOutputSampleBufferDelegate, CLLocationManagerDelegate {
     
+    @IBOutlet weak var takePhotoButton: UIButton!
     @IBOutlet weak var imageTake: UIImageView!
     var imagePicker: UIImagePickerController!
     var captureSession:AVCaptureSession!
+    var urlBaseLocationRestriction = "http://172.30.1.208:5002/"
+    var urlBaseRouter = "http://172.30.1.208:5001/"
+    let locationManager = CLLocationManager()
+    
+    var latitude = 0.0
+    var longitude = 0.0
+    var take_picture_on_next_location = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         prepareCaptureSession()
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if take_picture_on_next_location {
+            take_picture_on_next_location = false
+            takePhotoButton.setTitle("Take Photo", for: .normal)
+            let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+            print("locations = \(locValue.latitude) \(locValue.longitude)")
+            latitude = locValue.latitude
+            longitude = locValue.longitude
+            
+            if !UIImagePickerController.isSourceTypeAvailable(.camera){
+                let alertController = UIAlertController.init(title: nil, message: "Device has no camera.", preferredStyle: .alert)
+                let okAction = UIAlertAction.init(title: "Alright", style: .default, handler: {(alert: UIAlertAction!) in
+                })
+                alertController.addAction(okAction)
+                self.present(alertController, animated: true, completion: nil)
+            } else {
+                imagePicker =  UIImagePickerController()
+                imagePicker.delegate = self
+                imagePicker.sourceType = .camera
+                present(imagePicker, animated: true, completion: nil)
+            }
+        }
     }
     
     func prepareCaptureSession() {
@@ -33,22 +80,37 @@ class PhotoViewController: UIViewController, UINavigationControllerDelegate, UII
 
     //MARK: - Take image
     @IBAction func takePhoto(_ sender: UIButton) {
-        if !UIImagePickerController.isSourceTypeAvailable(.camera){
-            let alertController = UIAlertController.init(title: nil, message: "Device has no camera.", preferredStyle: .alert)
-            let okAction = UIAlertAction.init(title: "Alright", style: .default, handler: {(alert: UIAlertAction!) in
-            })
-            alertController.addAction(okAction)
-            self.present(alertController, animated: true, completion: nil)
-        } else {
-            imagePicker =  UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType = .camera
-            present(imagePicker, animated: true, completion: nil)
-        }
+        take_picture_on_next_location = true
+        // TODO disabled
+        takePhotoButton.setTitle("Géolocalisation en cours...", for: .normal)
     }
     
     //MARK: - Saving Image here
     @IBAction func sendImage(_ sender: AnyObject) {
+        if imageTake.image != nil {
+            let imageData = UIImageJPEGRepresentation(imageTake.image!, 1.0)!
+            
+            Alamofire.upload(imageData, to: "\(urlBaseRouter)picture/").responseJSON { response in
+                //debugPrint(response)
+                if let json = response.result.value {
+                    print("JSON: \(json)")
+                    let jsonObject = JSON(json)
+                    let message =  jsonObject["message"].stringValue
+                    let alertController = UIAlertController.init(title: nil, message: message, preferredStyle: .alert)
+                    let okAction = UIAlertAction.init(title: "Ok", style: .default, handler: {(alert: UIAlertAction!) in
+                    })
+                    alertController.addAction(okAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        } else {
+            let alertController = UIAlertController.init(title: nil, message: "Il faut prendre une photo avant de pouvoir l'envoyer.", preferredStyle: .alert)
+            let okAction = UIAlertAction.init(title: "Ok", style: .default, handler: {(alert: UIAlertAction!) in
+            })
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        
     }
     
     //MARK: - Add image to Library
@@ -70,6 +132,7 @@ class PhotoViewController: UIViewController, UINavigationControllerDelegate, UII
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         imagePicker.dismiss(animated: true, completion: nil)
         imageTake.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        
     }
 
 }
