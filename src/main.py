85374722e64c2c0af2386cc6efa236df2866bc09
Nicolas Lim <nicnl25@gmail.com>
2017-11-09 23:32:19 +0100

    Route d'inscription

--- src/main.py
+++ src/main.py
@@ -5,11 +5,16 @@ from json_responses import json_data, json_error, json_response
 import os
 import random
 import uuid
+import json
 import requests
 
 
 app = Flask(__name__)
 CORS(app)
+
+
+# Todo: URL paramétrable
+location_restriction_server = 'http://127.0.0.1:5002'
 
 
 @app.route('/picture/', methods=['POST'])
@@ -28,6 +33,38 @@ def google_vision():
     yolo_file.close()
 
     return json_response('Ça marche pas mais j\'ai pas envie de te dire pourqoi')
+
+
+@app.route('/team/', methods=['POST'])
+def register_team(lrid, team_name):
+    json_data = request.get_json(force=True)
+
+    lrid = json_data['lrid']
+    team_name = json_data['name']
+
+    # Todo: URL paramétrable
+    try:
+        response = requests.get(location_restriction_server + '/is_valid/inscription_retrait/%s' % lrid)
+    except requests.exceptions.ConnectionError:
+        return json_error('Cannot connect to LR server to verify LRID validity.')
+
+    if response.status_code != 200:
+        return json_error('Internal server error when verifying LRID validity.')
+
+    json_response = json.loads(response.content)
+    if not json_response['is_valid']:
+        return json_error('The provided LRID is invalid.')
+
+    # Todo: Inscrire la team en BDD
+    # Todo: Récupérer l'ID de la team en BDD ?
+    # Todo: Générer un token correspondant à la team
+    # Todo: Stocker ce token en BDD
+    # Todo: Renvoyer le token en json
+    # Todo: Envoyer l'ordre de création du container à l'orchestrator
+
+    return json_data({
+        'token': '89653832030e7d26daf3a43fc2ccd501'
+    })
 
 
 if __name__ == '__main__':
