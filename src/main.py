4abd31f94aff2323d5418a1921f70ca69e273400
Nicolas Lim <nicnl25@gmail.com>
2017-11-09 22:17:09 +0100

    Écriture de la photo envoyée par iOS sur le disque pour test

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from flask import Flask, request
 from flask_cors import CORS
+from json_responses import json_data, json_error, json_response
 import os
 import random
 import uuid
@@ -11,9 +12,17 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/hello/', methods=['GET'])
-def hello():
-    return 'salut'
+@app.route('/picture/', methods=['POST'])
+def google_vision():
+    raw_data = request.stream.read()
+
+    print('received post of length %d' % len(raw_data))
+
+    yolo_file = open('picture.jpeg', 'wb')
+    yolo_file.write(raw_data)
+    yolo_file.close()
+
+    return json_response('Ça marche pas mais j\'ai pas envie de te dire pourqoi')
 
 
 if __name__ == '__main__':
