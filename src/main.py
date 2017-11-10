133e7a1f7199bdd6f2b11d88cf29832fe7ae46ca
Nicolas Lim <nicnl25@gmail.com>
2017-11-10 17:27:34 +0100

    Suppression du token_generator, utilisation de redis pour récupération token et uuid depuis lrid-generator

--- src/main.py
+++ src/main.py
@@ -1,65 +1,47 @@
 # coding=utf-8
 from flask import Flask, request
 from flask_cors import CORS
-import os
-import random
-import uuid
-import requests
+import redis
+import json
 from json_responses import json_data, json_error, json_response
-from token_generator import TokenGenerator
-import copy
 
 # Initialisation Flask
 app = Flask(__name__)
 CORS(app)
 
-# Génération des UUIDs
-uuids = {
-    'inscription_retrait': None,
-    'depot': None
-}
-
-token_generators = copy.copy(uuids)
-regen_token_interval_secs = 3
-token_validity_secs = 30
-
-for key, value in uuids.items():
-    uuids[key] = uuid.uuid4().hex
-    uuids[key] = uuids[key][0:8] + '-' + uuids[key][8:12] + '-' + uuids[key][12:16] + '-' + uuids[key][16:20] + '-' + uuids[key][20:32]
-
-    token_generators[key] = TokenGenerator(
-        interval_secs=regen_token_interval_secs,
-        retain=int(token_validity_secs / regen_token_interval_secs)
-    )
-    token_generators[key].start()
-
-print(uuids)
+# Todo: Rendre configurable le serveur redis depuis ENV ?
+r = redis.StrictRedis(host='redis', port=6379, db=0)
 
 
 @app.route('/is_valid/<role>/<token>', methods=['GET'])
 def is_token_valid(role, token):
-    if role not in uuids:
+    if role not in ['inscription_retrait', 'depot']:
         return json_response('Unknown role.')
 
     return json_data({
-        'is_valid': token_generators[role].is_valid(token)
+        'is_valid': token in json.loads(r.get('lrid_%s_retained' % role).decode('utf-8'))
     })
 
 
 @app.route('/uuid/', methods=['GET'])
 def get_uuid():
-    return json_data(uuids)
+    return json_data({
+        'inscription_retrait': r.get('uuid_inscription_retrait').decode('utf-8'),
+        'depot': r.get('uuid_depot').decode('utf-8'),
+    })
 
 
 @app.route('/major-minor/<role>/', methods=['GET'])
 def get_major_minor(role):
+    lrid = r.get('lrid_%s_last' % role).decode('utf-8')
+
     return json_data({
-        'uuid': uuids[role],
-        'major': token_generators[role].token[0:4],
-        'minor': token_generators[role].token[4:8],
-        'interval_secs': regen_token_interval_secs
+        'uuid': r.get('uuid_%s' % role).decode('utf-8'),
+        'major': lrid[0:4],
+        'minor': lrid[4:8],
+        'interval_secs': int(r.get('lrid_regen_token_interval_secs').decode('utf-8'))
     })
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002)
+    app.run(host='0.0.0.0', port=5000)
