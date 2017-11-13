45991abeb2d2e0485da569bd10e2d2ebba1790e7
Nicolas Lim <nicnl25@gmail.com>
2017-11-13 10:57:54 +0100

    Orchestrator qui créé bien les containers à la volée

--- src/main.py
+++ src/main.py
@@ -1,15 +1,39 @@
 # coding=utf-8
 from flask import Flask, request
 from flask_cors import CORS
+from json_responses import json_data, json_error, json_response
+import docker
 
 # Initialisation Flask
 app = Flask(__name__)
 CORS(app)
 
+# Fixme: Virer cette ligne quand https://github.com/docker/docker-py/pull/1545 aura été mergé
+client = docker.from_env()
 
 @app.route('/')
 def increment():
     return 'Hello world, this is the orchestrator!'
+
+
+@app.route('/create/', methods=['POST'])
+def create_container():
+    data = request.get_json(force=True)
+
+    container_name = 'smart-scavenger-hunt-game-' + data['team_uuid']
+
+    client.containers.run(
+        image='smart-scavenger-hunt:game',
+        auto_remove=True,
+        remove=True,
+        name=container_name,
+        detach=True,
+        network='smart-scavenger-hunt'
+    )
+
+    return json_data({
+        'container_name': container_name
+    }, 201)
 
 
 if __name__ == '__main__':
