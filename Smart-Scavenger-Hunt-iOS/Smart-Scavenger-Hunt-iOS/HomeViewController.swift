990a5306c97b267fe89760b31f3ddd44d78fd2b2
afloury <antoine.floury@imerir.com>
2017-11-13 08:34:05 +0100

    Page d'accueil

--- Smart-Scavenger-Hunt-iOS/Smart-Scavenger-Hunt-iOS/HomeViewController.swift
+++ Smart-Scavenger-Hunt-iOS/Smart-Scavenger-Hunt-iOS/HomeViewController.swift
@@ -1,35 +1,46 @@
-//
-//  HomeViewController.swift
-//  Smart-Scavenger-Hunt-iOS
-//
-//  Created by student on 08/11/2017.
-//  Copyright © 2017 student. All rights reserved.
-//
-
 import UIKit
+import KeychainSwift
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let keychain = KeychainSwift()
+    
+    @IBOutlet weak var titleTop: UILabel!
+    @IBOutlet weak var messageTitle: UILabel!
+    @IBOutlet weak var titleTableV: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        //print("viewDidAppear")
+        if let token = keychain.get("token") {
+            titleTop.text = "Hey"
+            messageTitle.text = "Go to the withdrawal point to get your mission"
+            titleTableV.isHidden = false
+            tableView.isHidden = false
+        } else {
+            titleTop.text = "Hello"
+            messageTitle.text = "Welcome to Scavenger Hunt\nto start a session go near to registration point"
+            // TODO Uncomment
+            titleTableV.isHidden = true
+            tableView.isHidden = true
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "myCell")
+        cell.textLabel?.text = "hey"
+        cell.detailTextLabel?.text = "ho"
+        return cell
+    }
+    
 }
