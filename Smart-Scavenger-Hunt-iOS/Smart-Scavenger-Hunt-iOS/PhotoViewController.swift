6ba23837ec070cb0c171914b410632d303cba667
afloury <antoine.floury@imerir.com>
2017-11-13 08:34:44 +0100

    Better UX while Taking photo

--- Smart-Scavenger-Hunt-iOS/Smart-Scavenger-Hunt-iOS/PhotoViewController.swift
+++ Smart-Scavenger-Hunt-iOS/Smart-Scavenger-Hunt-iOS/PhotoViewController.swift
@@ -16,11 +16,12 @@ class PhotoViewController: UIViewController, UINavigationControllerDelegate, UII
     
     var latitude = 0.0
     var longitude = 0.0
-    var take_picture_on_next_location = false
+    var listenNextLocation = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        takePhotoButton.setTitle("Géolocalisation en cours...", for: .disabled)
         // Do any additional setup after loading the view.
         prepareCaptureSession()
         // Ask for Authorisation from the User.
@@ -38,26 +39,13 @@ class PhotoViewController: UIViewController, UINavigationControllerDelegate, UII
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if take_picture_on_next_location {
-            take_picture_on_next_location = false
+        if listenNextLocation {
+            listenNextLocation = false
             takePhotoButton.setTitle("Take Photo", for: .normal)
             let locValue:CLLocationCoordinate2D = manager.location!.coordinate
             print("locations = \(locValue.latitude) \(locValue.longitude)")
             latitude = locValue.latitude
             longitude = locValue.longitude
-            
-            if !UIImagePickerController.isSourceTypeAvailable(.camera){
-                let alertController = UIAlertController.init(title: nil, message: "Device has no camera.", preferredStyle: .alert)
-                let okAction = UIAlertAction.init(title: "Alright", style: .default, handler: {(alert: UIAlertAction!) in
-                })
-                alertController.addAction(okAction)
-                self.present(alertController, animated: true, completion: nil)
-            } else {
-                imagePicker =  UIImagePickerController()
-                imagePicker.delegate = self
-                imagePicker.sourceType = .camera
-                present(imagePicker, animated: true, completion: nil)
-            }
         }
     }
     
@@ -80,13 +68,27 @@ class PhotoViewController: UIViewController, UINavigationControllerDelegate, UII
 
     //MARK: - Take image
     @IBAction func takePhoto(_ sender: UIButton) {
-        take_picture_on_next_location = true
-        // TODO disabled
-        takePhotoButton.setTitle("Géolocalisation en cours...", for: .normal)
+        //takePhotoButton.isEnabled = false
+        if !UIImagePickerController.isSourceTypeAvailable(.camera){
+            let alertController = UIAlertController.init(title: nil, message: "Device has no camera.", preferredStyle: .alert)
+            let okAction = UIAlertAction.init(title: "Alright", style: .default, handler: {(alert: UIAlertAction!) in
+                //self.takePhotoButton.isEnabled = true
+            })
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+        } else {
+            imagePicker =  UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera
+            present(imagePicker, animated: true, completion: nil)
+        }
     }
     
-    //MARK: - Saving Image here
     @IBAction func sendImage(_ sender: AnyObject) {
+        sendPhoto()
+    }
+    
+    func sendPhoto() {
         if imageTake.image != nil {
             let resizedImage = imageTake.image!.resized(toWidth: 800)
             let imageData = UIImageJPEGRepresentation(resizedImage!, 0.75)!
@@ -111,29 +113,13 @@ class PhotoViewController: UIViewController, UINavigationControllerDelegate, UII
             alertController.addAction(okAction)
             self.present(alertController, animated: true, completion: nil)
         }
-        
-    }
-    
-    //MARK: - Add image to Library
-    @objc
-    func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-        if let error = error {
-            // we got back an error!
-            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
-            present(ac, animated: true)
-        } else {
-            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
-            present(ac, animated: true)
-        }
     }
     
     //MARK: - Done image capture here
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         imagePicker.dismiss(animated: true, completion: nil)
         imageTake.image = info[UIImagePickerControllerOriginalImage] as? UIImage
-        
+        listenNextLocation = true
+        takePhotoButton.setTitle("Retake Photo", for: .normal)
     }
-
 }
