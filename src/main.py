256a2c41c057074774a6aedf6e4bab2a4f59b42f
Nicolas Lim <nicnl25@gmail.com>
2017-11-15 19:10:05 +0100

    Ajout routes des scores

--- src/main.py
+++ src/main.py
@@ -2,23 +2,33 @@
 from flask import Flask, request
 from flask_cors import CORS
 from json_responses import json_data, json_error, json_response
+import json
+import redis
 
 # Initialisation Flask
 app = Flask(__name__)
 CORS(app)
 
-
-@app.route('/')
-def increment():
-    return 'Hello world, this is the orchestrator!'
+# Todo: Rendre configurable le serveur redis depuis ENV ?
+r = redis.StrictRedis(host='redis', port=6379, db=0)
 
 
-@app.route('/hello/', methods=['GET'])
+@app.route('/score/', methods=['GET'])
 def create_container():
-    return json_data({
-        'message': 'Salut mec !'
-    })
+    raw_teams = r.get('teams')
+    if raw_teams is None:
+        return json_data([])
+
+    scores = []
+    for team_uuid in json.loads(raw_teams.decode('utf-8')):
+        team_data = json.loads(r.get('team-' + team_uuid).decode('utf-8'))
+        scores += [{
+            'name': team_data['name'],
+            'score': team_data['score']
+        }]
+
+    return json_data(scores)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5005, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
