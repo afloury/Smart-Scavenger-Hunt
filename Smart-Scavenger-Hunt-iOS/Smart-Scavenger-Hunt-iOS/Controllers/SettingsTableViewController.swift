13dc9f0f64321cce62310e8b7dfe345bffb8d44b
afloury <antoine.floury@imerir.com>
2017-11-16 16:57:30 +0100

    Leave mission and better UX in settings

--- Smart-Scavenger-Hunt-iOS/Smart-Scavenger-Hunt-iOS/Controllers/SettingsTableViewController.swift
+++ Smart-Scavenger-Hunt-iOS/Smart-Scavenger-Hunt-iOS/Controllers/SettingsTableViewController.swift
@@ -5,16 +5,24 @@ class SettingsTableViewController: UITableViewController {
 
     let keychain = KeychainSwift()
     var numberOfSection = 1
+    var homeVC: HomeViewController!
+    let api = API()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let home = tabBarController?.viewControllers![0] as? HomeViewController {
+            homeVC = home
+        }
+    }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         if keychain.get("token") != nil {
             numberOfSection = 2
-            tableView.reloadData()
         } else {
             numberOfSection = 1
-            tableView.reloadData()
         }
+        tableView.reloadData()
     }
 
     // MARK: - Table view data source
@@ -26,19 +34,33 @@ class SettingsTableViewController: UITableViewController {
         if section == 0 {
             return 3
         }
+        if homeVC.hadMission {
+            return 2
+        }
         return 1
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.section == 1 && indexPath.row == 0 {
-            print("disconnect")
-            keychain.clear()
-            numberOfSection = 1
-            tableView.reloadData()
-            if let home = tabBarController?.viewControllers![0] as? HomeViewController {
-                home.hadMission = false
-            }
-            tabBarController?.selectedIndex = 0
+            // Disconnect Team
+            Alert.show(controller: self, message: "Disconnect your team ?", okAction: UIAlertAction.init(title: "Yes", style: .default, handler: { (alert: UIAlertAction!) in
+                self.keychain.clear()
+                self.numberOfSection = 1
+                self.homeVC.hadMission = false
+                self.tabBarController?.selectedIndex = 0
+                tableView.reloadData()
+            }), cancelAction: true)
+        } else if indexPath.section == 1 && indexPath.row == 1 {
+            // Leave mission (penalty)
+            
+            Alert.show(controller: self, message: "Are you sure to leave the current mission ?", okAction: UIAlertAction.init(title: "Yes", style: .default, handler: {(alert: UIAlertAction!) in
+                self.api.leaveMission()
+                self.homeVC.hadMission = false
+                tableView.reloadData()
+                Alert.show(controller: self, message: "Your team loose 3 points ☹️", title: "Mission left", okAction: UIAlertAction.init(title: "Ok", style: .default, handler: {(alert: UIAlertAction!) in
+                    self.tabBarController?.selectedIndex = 0
+                }))
+            }), cancelAction: true)
         }
     }
 }
